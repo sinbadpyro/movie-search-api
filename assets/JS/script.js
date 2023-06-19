e9ca82367639b1888e43838f65dd3627b7a05715
sinbadpyro <omorodionsinbad@gmail.com>
2023-06-19 22:52:46 +0200

    Tried adding new functions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,16 +6,19 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTBjM2QyYjhmNGJlZGExZTU3NGM3NzNlOWYyOTQwZCIsInN1YiI6IjYzZWZiZTZkMzU4MThmMDBkYjNkODgzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5M1G0z-dbMLtvz74RS7RTSRZuBzvjut92JvBxm1cBiI",
   },
 };
-
+//Les variables
 let bouton = document.getElementById("bouton");
 let contain = document.getElementById("movie-result");
 let mySearch = document.getElementById("mySearch");
+let aboutcontain = document.getElementById("movie-show");
 let synopsis = document.getElementById("movie-overview");
+let input = document.getElementById("mySearch").value;
 //ceci est de monntrer les films quand on clicker sur le bouton
 bouton.addEventListener("click", function () {
   displayrequest();
 });
 //ceci est de montrer les films quand on appuie sur entrer
+
 mySearch.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     console.log("pressed");
@@ -27,13 +30,15 @@ mySearch.addEventListener("keydown", function (e) {
 function displayrequest() {
   let input = document.getElementById("mySearch").value;
   let search = input;
+
   //cette condition se deroule quand le utilasateur ne mets pas un donnee
+
   if (search === "") {
     console.log("pls enter a film");
     contain.innerHTML = `<div class="contain">
      <h4>PLEASE ENTER A FILM...</h4> 
    </div>`;
-    //
+    /***************/
   }
   console.log(search);
 
@@ -51,7 +56,7 @@ function displayrequest() {
          <div class="car m-2 col-lg-3 px-0">
          <img src="https://image.tmdb.org/t/p/w500/${element.backdrop_path}" alt="Avatar" style="width:100%">
          <div class="contain">
-         <h4 id="movie-overview"><a href="about.html">Title: <b>${element.original_title}</b></a></h4> 
+         <h4><a href="about.html" id="movie-overview">Title: <b>${element.original_title}</b></a></h4> 
            <h4>Date de Sortie: ${element.release_date}</h4> 
            <h4>Ratings: ${element.vote_average}</h4> 
          </div>
@@ -63,21 +68,28 @@ function displayrequest() {
     .catch((err) => console.error(err));
 }
 /****************************************************************************************** */
-//for more details about the movie
+//for more details about the movie, and to go to another page.
 
-function overView() {
+/*****This is a function that saves the text when we go to another page */
+function saveText() {
   let input = document.getElementById("mySearch").value;
-  let search = input;
-  //cette condition se deroule quand le utilasateur ne mets pas un donnee
-  if (search === "") {
-    console.log("pls enter a film");
-    contain.innerHTML = `<div class="contain">
-     <h4>PLEASE ENTER A FILM...</h4> 
-   </div>`;
-    //
-  }
-  console.log(search);
 
+  if (sessionStorage.getItem("autosave")) {
+    // Restore the contents of the text field
+    input = sessionStorage.getItem("autosave");
+  }
+
+  // Listen for changes in the text field
+  input.addEventListener("change", () => {
+    // And save the results into the session storage object
+    sessionStorage.setItem("autosave", input);
+  });
+  window.location = "about.html";
+  return false;
+}
+
+function displayMovieDetails() {
+  saveText();
   fetch(
     "https://api.themoviedb.org/3/search/movie?query=" +
       search +
@@ -88,12 +100,14 @@ function overView() {
     .then((data) => {
       console.log(data);
       data.results.forEach((element) => {
-        contain.innerHTML += `
-         <div class="car m-2 col-lg-6 px-0">
+        aboutcontain.innerHTML = `
+         <div class="car m-2 col-lg-3 px-0">
          <img src="https://image.tmdb.org/t/p/w500/${element.backdrop_path}" alt="Avatar" style="width:100%">
          <div class="contain">
-         <h4><a href="about.html">Title: <b>${element.original_title}</b></a></h4> 
-           <p>${element.overview}</p>
+         <h4><a href="about.html" id="movie-overview">Title: <b>${element.original_title}</b></a></h4> 
+           <h4>Date de Sortie: ${element.release_date}</h4> 
+           <h4>Ratings: ${element.vote_average}</h4> 
+           <h3>premise: ${element.overview}</h3>
          </div>
        </div>
            
@@ -102,7 +116,5 @@ function overView() {
     })
     .catch((err) => console.error(err));
 }
-bouton.addEventListener("load", function () {
-  overView();
-});
+
 /****************************************************************************************** */
